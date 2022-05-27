d8d8bd8df2efa466487375438c8f1f8b34659400
saadbouhya <bouhyasaad@gmail.com>
2022-05-27 15:39:50 +0200

    fix python script

--- gps/test_python/script.py
+++ gps/test_python/script.py
@@ -2,12 +2,12 @@
 import serial
 import time
 
-serial_port = serial.Serial("/dev/ttyS0",115200)
+serial_port = serial.Serial("/dev/ttyS0", 115200)
 
-w_buff = ["AT+CGNSPWR=1\r\n", "AT+CGNSSEQ=\"RMC\"\r\n", "AT+CGNSINF\r\n", "AT+CGNSINF\r\n", "AT+CGNSURC=2\r\n", "AT+CGNSTST=1\r\n"]
+w_buff = ["AT+CGNSPWR=1\r\n", "AT+CGNSSEQ=\"RMC\"\r\n", "AT+CGNSINF\r\n", "AT+CGNSURC=2\r\n", "AT+CGNSTST=1\r\n"]
 
 serial_port.write(w_buff[0])
-serial_port.flush()
+serial_port.flushInput()
 
 data = ""
 num = 0
@@ -18,6 +18,15 @@ try:
             data += serial_port.read(serial_port.inWaiting())
         if data != "":
             print data
+            time.sleep(0.5)
+            serial_port.write(w_buff[num+1])
+            num = num+1
+            
+            if num == 4:
+                time.sleep(0.5)
+                serial_port.write(w_buff[4])
+
+            data = ""
 except KeyboardInterrupt:
     if serial_port != None:
         serial_port.close()
